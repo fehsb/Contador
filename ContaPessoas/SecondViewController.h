2726ce619a71c31c58e345ab9da668254574b646
Fernando Baptista <fernandohsb@gmail.com>
2015-02-19 14:25:51 -0200

    oi

--- ContaPessoas/SecondViewController.h
+++ ContaPessoas/SecondViewController.h
@@ -13,7 +13,8 @@
 @property (weak, nonatomic) IBOutlet UILabel *totalBoys;
 @property (weak, nonatomic) IBOutlet UILabel *totalGirls;
 @property (weak, nonatomic) IBOutlet UILabel *total;
-- (IBAction)click:(id)sender;
+- (IBAction)click:(id)sender; // oi?
+
 
 @end
 
